80a45e838f00515316da739f7999d0d1b35e8366
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-02-17 11:30:20 +0900

    修正3

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: :index
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @prototype = Prototype.all
@@ -52,12 +51,6 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-      if user_signed_in?
-        redirect_to action: :index
-      end
   end
 
 end
