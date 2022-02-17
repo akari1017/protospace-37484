4a94be14141fd1bdfe067665f3affbd87990107e
akari1017 <98454251+akari1017@users.noreply.github.com>
2022-02-17 13:43:47 +0900

    修正8

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,13 +25,12 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    if user_signed_in?
-      @prototype = Prototype.find(params[:id])
-    else
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to root_path
     end
   end
-
+  
   def update
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
